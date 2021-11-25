1821e757148616932d6ac0e24b280ee284d17327
Ian Zurutuza <ian.zurutuza@gmail.com>
2021-11-24 19:49:09 -0600

    updated kmeans select by mask

--- python/sklearn_img_kmeans.py
+++ python/sklearn_img_kmeans.py
@@ -10,33 +10,18 @@ from sklearn.cluster import KMeans
 
 
 #read in picture
-path = '/home/ian/Documents/UNT/wearables/burn recognition/segmentation.jpg'
+path = '/home/ian/Downloads/burned_baby.png'
 img = np.asarray(Image.open(path))
 img = img[:,:,:3]
 flat_img = img.reshape((-1, 3))
 
-# 1st Stage K Means with 64 colors
-clt = KMeans(64)
+# K Means with 16 colors
+clt = KMeans(16)
 clt.fit(flat_img)
 cluster_centers = clt.cluster_centers_
 cluster_labels = clt.labels_
 
-k64 = cluster_centers[cluster_labels]
-k64_norm = (cluster_centers[cluster_labels].reshape(img.shape))/255
-
-# Plot image with 64 colors
-plt.figure("64")
-plt.axis("off")
-plt.title("K = 64, Step = 1")
-plt.imshow(k64_norm)
-plt.show()
-
-# 2nd Stage K Means with 16 colors
-clt = KMeans(16)
-clt.fit(k64)
-cluster_centers = clt.cluster_centers_
-cluster_labels = clt.labels_
-
+#mapping cluster center values to labels
 k16 = cluster_centers[cluster_labels]
 k16_norm = (cluster_centers[cluster_labels].reshape(img.shape))/255
 
@@ -44,18 +29,47 @@ k16_norm = (cluster_centers[cluster_labels].reshape(img.shape))/255
 # Plot image with 16 colors
 plt.figure("16")
 plt.axis("off")
-plt.title("K = 16, Step = 2")
+plt.title("K = 16")
 plt.imshow(k16_norm)
 plt.show()
 
-mask_1 = cluster_centers[6,:]
-k16_r = np.copy(k16)
-k16_r = np.where((mask_1 == k16), 0, k16_r)
+#Mask creation
+mask_Set = []
+mask_img =[]
 
-k16_r = (k16_r.reshape(img.shape))/255
-# Plot masked image
-plt.figure("mask")
-plt.axis("off")
-plt.title("mask of n=16 step =2 image")
-plt.imshow(k16_r)
+for i in range(len(cluster_centers)):
+    mask_Set.append(np.where(cluster_labels == i, 1,0).reshape((img.shape[0],img.shape[1])))
+    mask = np.moveaxis([mask_Set[-1],mask_Set[-1],mask_Set[-1]],0,-1)
+    mask_img.append(mask * img)
+
+#Plot individual masks
+    fig = plt.figure()
+for i in range(1, (len(mask_img)+1)):
+    ax = fig.add_subplot(4, 4,i)
+    ax.set_axis_off()
+    plt.imshow(mask_img[i-1])
+    ax.set_title('Mask ' + str(i))
+plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
 plt.show()
+
+#user to define content of masked image
+mask_sbo = np.zeros((3,img.shape[0],img.shape[1],img.shape[2]))
+for i in range(len(cluster_centers)):
+    sbo = int(input("Is mask "+str(i) +" 0-skin, 1-burn, 2-other"))
+    mask_sbo[sbo] +=mask_img[i]
+
+#plotting of skin, burn and other combined mask
+names=['skin', 'burn','other']
+for i in range(len(mask_sbo)):
+    plt.figure("mask" + names[i])
+    plt.axis("off")
+    plt.title("Mask " + names[i])
+    plt.imshow(np.uint8(mask_sbo[i]))
+    plt.show()
+sbo_sum = []
+
+#calculating % burn based on combined mask
+for m in mask_sbo:
+    sbo_sum.append(np.sum(np.nonzero(m))/3)
+per_brun = sbo_sum[1]/(sbo_sum[0]+sbo_sum[1])*100
+print('Percent Burn: '+str(per_brun))
